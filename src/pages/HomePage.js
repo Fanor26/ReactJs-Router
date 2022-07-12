1290b83bf23b85c13d104c8fa006fdc144051521
FANOR26 <87057752+Fanor26@users.noreply.github.com>
2022-07-12 06:02:04 +0000

    useCoontext+useForm && yarn add @material-ui/core

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,39 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../hooks/useContext'
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import { useStyles, BootstrapInput } from '../components/subcomponents/Input';
+import useForm from '../hooks/userForm';
+import { Input } from '@material-ui/core';
 export const HomePage = () => {
-    const {user} =useContext(UserContext)
+  const classes = useStyles();
+    const {user, setUser} =useContext(UserContext)
+    const [form, handlerChangeForm, handlerResetForm] = useForm({username:""})
+    const {username}= form;
+    const onSubmit = (e) => {
+      e.preventDefault();
+      //const updateUser={username: username,...user}
+      const updateUser={...user, username}
+      //console.log(updateUser)
+      setUser(updateUser)
+    };
     return (
         <div>
         <h2>HomePage {user.username}</h2>
+        <form className={classes.root} onSubmit={onSubmit}>
+      
+
+        
+        <BootstrapInput 
+        placeholder="username"
+       name="username" 
+        value={username} 
+        onChange={handlerChangeForm}
+    />
+    {/*
+      <input type="text" name="username"  value={username} onChange={handlerChangeForm}/>
+    */}
+    </form>
         </div>
     )
 }
